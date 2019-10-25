bc7acad12cced783e1e9e5d987033b012e5a0f18
fujianxin123456 <abcfjx123456@sina.com>
2019-10-25 22:49:06 +0800

    sort_by_exchange add commit

--- sort_by_exchange.cpp
+++ sort_by_exchange.cpp
@@ -1,19 +1,21 @@
-//‰∫§Êç¢ÊéíÂ∫è
+//Ωªªª≈≈–Ú
 #include <stdio.h>
-int main() {
-  int x[3] = {5, 2, 1}, i;
-  while(1) {
+int main() 
+{
+    int x[3], i,j;
     scanf("%d %d %d", &x[0], &x[1], &x[2]);
-    for(i=0;i<2;i++) {
-      if( x[i] > x[i+1]) {
-  	    int temp = x[i];
-  	    x[i] = x[i+1];
-  	    x[i+1] = temp;
-      }
-      i = i + 1;
-      i = i - 1;
+    for(j = 0; j < 2; j++)
+	{
+		for(i = 0;i < 2 - j;i++)
+		{
+         if( x[i] > x[i+1])
+		 {
+		    int temp = x[i];
+  	        x[i] = x[i+1];
+  	        x[i+1] = temp;
+	     }
+        }
     }
     printf(" %d, %d, %d", x[0], x[1], x[2]);
-  }
-  return 0;
+    return 0;
 }
